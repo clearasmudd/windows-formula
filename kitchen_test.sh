10bf2f932f13837e962e30eca287ca3f04fdf9bf
Peter Mudd <muddman@users.noreply.github.com>
2020-02-07 11:14:42 -0500

    chore: cleanup

--- kitchen_test.sh
+++ kitchen_test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # exit when any command fails
 
+git diff-index --quiet HEAD -- || { echo "Exiting, you have uncommitted git changes."; git status; exit 1; } 
+
+./lint.sh
+
 echo "** Removing the following test kitchen results:"
 find test/results/ -type f -iregex '.*[json|txt|xml|log]'
 find test/results/ -type f -iregex '.*[json|txt|xml|log]' -delete
-./lint.sh
-
-set -e
 
 echo "** Running test kitchen against Windows 10 locally"
 for SERVER in $(kitchen list | awk '{if(NR>1)print $1}')
@@ -14,18 +15,24 @@ do
   my_log="test/results/$SERVER.log"
   if [[ $SERVER == *"windows-10-1903"* ]]; then
     date | tee $my_log
-    echo "** git log HEAD~3..HEAD" | tee -a $my_log
-    git log HEAD~3..HEAD | tee -a $my_log
-    echo "** git status:" | tee -a $my_log
-    git status | tee -a $my_log
-    echo "** unning test kitchen against $SERVER." | tee -a $my_log
+    set -ex
+    git log HEAD~2..HEAD | tee -a $my_log
+    # git status | tee -a $my_log
     kitchen test $SERVER -l info | tee -a $my_log
-    echo "** git status:" | tee -a $my_log
     git status | tee -a $my_log
   fi
   # git add test/results/*
   # git commit -m "test(kitchen): run local test kitchen."
-  echo "** verify and then: "
-  echo "git add test/results/*"
-  echo "git commit -m 'test(kitchen): run lint and local test kitchen for Windows 10 hosts'"
+  read -p "Commit test results and push to github? [y]" -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    git add test/results/*
+    git commit -m 'test(local): local `kitchen test` results.'
+    git push
+  else 
+    echo "git add test/results/*"
+    echo "git commit -m 'test(local): results from local `kitchen test`.'"
+    echo "git push"
+  fi
 done
