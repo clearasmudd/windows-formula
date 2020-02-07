afd679f995ae0d11e3c3728854754c5bb1d1fbe4
Peter Mudd <muddman@users.noreply.github.com>
2020-02-07 11:27:22 -0500

    test: script update

--- kitchen_test.sh
+++ kitchen_test.sh
@@ -9,7 +9,7 @@ echo "** Removing the following test kitchen results:"
 find test/results/ -type f -iregex '.*[json|txt|xml|log]'
 find test/results/ -type f -iregex '.*[json|txt|xml|log]' -delete
 
-echo "** Starting `kitchen test`"
+echo "** Starting kitchen test"
 for SERVER in $(kitchen list | awk '{if(NR>1)print $1}')
 do
   my_log="test/results/$SERVER.log"
@@ -22,16 +22,16 @@ do
   fi
   # git add test/results/*
   # git commit -m "test(kitchen): run local test kitchen."
-  read -p "Commit test results and push to github? [y]" -n 1 -r
+  read -p 'Commit test results and push to github? [y]' -n 1 -r
   echo    # (optional) move to a new line
   if [[ $REPLY =~ ^[Yy]$ ]]
   then
     git add test/results/*
-    git commit -m 'test(local): local `kitchen test` results.'
+    git commit -m "test(local): local 'kitchen test' results."
     git push
   else 
     echo "git add test/results/*"
-    echo "git commit -m 'test(local): results from local `kitchen test`.'"
+    echo "git commit -m \"test(local): local 'kitchen test' results.\""
     echo "git push"
   fi
 done
