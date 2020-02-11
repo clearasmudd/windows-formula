fbfe8bd68c8244fe211ed488c37e61350f557947
Peter Mudd <muddman@users.noreply.github.com>
2020-02-11 12:27:55 -0500

    chore: test runs successfully?

--- scripts/git-silent-uninstall.ps1
+++ scripts/git-silent-uninstall.ps1
@@ -1,4 +1,4 @@
-# source: https://github.com/Limech/git-powershell-silent-install/blob/master/git-silent-uninstall.ps1
+# based on: https://github.com/Limech/git-powershell-silent-install/blob/master/git-silent-uninstall.ps1
 ## There can be many places where git could have been installed.  Check likely places.
 ### List of likely places where Git could be 
 $possibleInstalledPaths = @("C:\Program Files\Git\", "C:\Program Files (x64)\Git\", "c:\git\", "C:\Program Files\Git LFS")
@@ -7,38 +7,46 @@ $foundAnInstallation = $false
 ### For all places where Git "could" be.
 foreach ($installPath in $possibleInstalledPaths)
 {
-    
-    ### If the path where Git could be exists
+  ### If the path where Git could be exists
+  if (Test-Path($installPath))
+  {
+    ## Some Git stuff might be running.. kill them.
+    Stop-Process -processname Bash -erroraction 'silentlycontinue'
+    Stop-Process -processname Putty* -erroraction 'silentlycontinue'
+    $foundAnInstallation = $true
+    Write-Host "Removing Git from " $installPath
+    ### Find if there's an uninstaller in the folder.
+    $uninstallers = Get-ChildItem $installPath"\unins*.exe"
+    ### In reality, there should only be just one that matches.
+    foreach ($uninstaller in $uninstallers)
+    {
+      ### Invoke the uninstaller.
+      $uninstallerCommandLineOptions = "/SP- /VERYSILENT /SUPPRESSMSGBOXES /FORCECLOSEAPPLICATIONS"
+      Start-Process -Wait -FilePath $uninstaller -ArgumentList $uninstallerCommandLineOptions
+    }
+    ### Remove the folder if it didn't clean up properly.
     if (Test-Path($installPath))
     {
-
-        ## Some Git stuff might be running.. kill them.
-        Stop-Process -processname Bash -erroraction 'silentlycontinue'
-        Stop-Process -processname Putty* -erroraction 'silentlycontinue'
-
-        $foundAnInstallation = $true
-        Write-Host "Removing Git from " $installPath
-
-        ### Find if there's an uninstaller in the folder.
-        $uninstallers = Get-ChildItem $installPath"\unins*.exe"
-
-        ### In reality, there should only be just one that matches.
-        foreach ($uninstaller in $uninstallers)
-        {
-           ### Invoke the uninstaller.
-           $uninstallerCommandLineOptions = "/SP- /VERYSILENT /SUPPRESSMSGBOXES /FORCECLOSEAPPLICATIONS"
-           Start-Process -Wait -FilePath $uninstaller -ArgumentList $uninstallerCommandLineOptions
-        }
-
-        ### Remove the folder if it didn't clean up properly.
-        if (Test-Path($installPath))
-        {
-           Remove-Item -Recurse -Force $installPath
-        }
+      Remove-Item -Recurse -Force $installPath
     }
+  }
 }
-
+# Add confirmation
 if (!($foundAnInstallation))
 {
    Write-Host "No git installation found. Nothing to uninstall"
+}
+else
+{
+  $confirmation = Get-Package -Provider Programs -IncludeWindowsInstaller -Name "Git*" -ErrorAction:SilentlyContinue
+  if ($confirmation)
+  {
+    $host.SetShouldExit($LastExitCode)
+    write-host $confirmation
+    throw "Git not uninstalled"
+  }
+  else
+  {
+    write-host "All Git instances successfully uninstalled"
+  }
 }
