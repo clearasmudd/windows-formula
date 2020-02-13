7026d51836a4c34aed529a4423661db98a9745da
Peter Mudd <muddman@users.noreply.github.com>
2020-02-12 21:59:26 -0500

    test: this it?

--- scripts/appveyor_tests.sh
+++ scripts/appveyor_tests.sh
@@ -64,7 +64,7 @@ function end_test {
     echo "APPVEYOR_TEST[cout]: ${APPVEYOR_TEST[cout]}"
     if [[ ! -z "${APPVEYOR_TEST[cout]}" ]]; then echo "XXXXXXXXXXX -StdOut"; fi
     echo "$(if [[ ! -z \"${APPVEYOR_TEST[cout]}\" ]]; then echo \"${APPVEYOR_TEST[cout]}\"; fi)"
-    # appveyor UpdateTest -Name "${APPVEYOR_TEST[name]}" -Framework "${APPVEYOR_TEST[framework]}" -Filename "${APPVEYOR_TEST[filename]}" -Duration "${APPVEYOR_TEST[cruntime]}" -Outcome Passed `if [ "${APPVEYOR_TEST[cout]}" != 'false' ]; then echo "-StdOut"; fi` "$(if [ \"${APPVEYOR_TEST[cout]}\" != 'false' ]; then echo \"${APPVEYOR_TEST[cout]}\"; fi)"
+    appveyor UpdateTest -Name "${APPVEYOR_TEST[name]}" -Framework "${APPVEYOR_TEST[framework]}" -Filename "${APPVEYOR_TEST[filename]}" -Duration "${APPVEYOR_TEST[cruntime]}" -Outcome Passed `if [[ ! -z "${APPVEYOR_TEST[cout]}" ]]; then echo "-StdOut"; fi` "$(if [[ ! -z \"${APPVEYOR_TEST[cout]}\" ]]; then echo \"${APPVEYOR_TEST[cout]}\"; fi)"
   else
     echo "${APPVEYOR_TEST[name]} did not complete successfully!  Check the 'Tests' tab in appveyor for additional information."
     appveyor UpdateTest -Name "${APPVEYOR_TEST[name]}" -Framework "${APPVEYOR_TEST[framework]}" -Filename "${APPVEYOR_TEST[filename]}" -Duration "${APPVEYOR_TEST[cruntime]}" -Outcome Failed -ErrorMessage "return code: ${APPVEYOR_TEST[cret]}" -StdOut "${APPVEYOR_TEST[cout]}" -StdErr "${APPVEYOR_TEST[cerr]}"
