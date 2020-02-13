8403d18f2b4111eca7cf7a8c50a757fdc68fa2b1
Peter Mudd <muddman@users.noreply.github.com>
2020-02-12 20:32:09 -0500

    test: how is this?

--- scripts/appveyor_tests.sh
+++ scripts/appveyor_tests.sh
@@ -39,7 +39,7 @@ function start_test {
   eval "$({ cerr=$({ cout=$(${APPVEYOR_TEST[command]}); cret=$?; } 2>&1; declare -p cout cret >&2); declare -p cerr; } 2>&1)"
   local end=`date +%s`
   APPVEYOR_TEST[cruntime]=$((end-start))
-  APPVEYOR_TEST[cret]="${cret:-''}"
+  APPVEYOR_TEST[cret]=$cret
   APPVEYOR_TEST[cout]="${cout:-''}"
   APPVEYOR_TEST[cerr]="${cerr:-''}"
   echo "${APPVEYOR_TEST[name]} finished in ${APPVEYOR_TEST[cruntime]} seconds with return code: ${APPVEYOR_TEST[cret]}"
@@ -58,7 +58,8 @@ function end_test {
     echo "${APPVEYOR_TEST[name]} completed successfully!"
     # cout_arg=$(if [ ${APPVEYOR_TEST[cout]} ]; then echo -StdOut "${APPVEYOR_TEST[cout]}"; fi)
     # updatetest="appveyor UpdateTest -Name ${APPVEYOR_TEST[name]} -Framework ${APPVEYOR_TEST[framework]} -Filename ${APPVEYOR_TEST[filename]} -Duration ${APPVEYOR_TEST[cruntime]} -Outcome Passed ${APPVEYOR_TEST[cout_arg]}"
-    appveyor UpdateTest -Name "${APPVEYOR_TEST[name]}" -Framework "${APPVEYOR_TEST[framework]}" -Filename "${APPVEYOR_TEST[filename]}" -Duration "${APPVEYOR_TEST[cruntime]}" -Outcome Passed $( (( ${APPVEYOR_TEST[cout]}  )) && printf %s "-StdOut ${APPVEYOR_TEST[cout]}" )
+    # `if [ ${APPVEYOR_TEST[cout]} ]; then echo "-StdOut ${APPVEYOR_TEST[cout]}"; fi`
+    appveyor UpdateTest -Name "${APPVEYOR_TEST[name]}" -Framework "${APPVEYOR_TEST[framework]}" -Filename "${APPVEYOR_TEST[filename]}" -Duration "${APPVEYOR_TEST[cruntime]}" -Outcome Passed $( (( ${APPVEYOR_TEST[cout]} != ''  )) && printf %s "-StdOut ${APPVEYOR_TEST[cout]}" )
   else
     echo "${APPVEYOR_TEST[name]} did not complete successfully!  Check the 'Tests' tab in appveyor for additional information."
     # updatetest="appveyor UpdateTest -Name ${APPVEYOR_TEST[name]} -Framework ${APPVEYOR_TEST[framework]} -Filename ${APPVEYOR_TEST[filename]} -Duration ${APPVEYOR_TEST[cruntime]} -Outcome Failed -ErrorMessage ${dqt}${APPVEYOR_TEST[name]} return code: ${APPVEYOR_TEST[cret_arg]}${dqt} ${APPVEYOR_TEST[cout_arg]} ${APPVEYOR_TEST[cerr_arg]}"
