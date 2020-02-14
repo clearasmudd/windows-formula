3f73d47ea6efcf18015b2003d17c4a243cf44c30
Peter Mudd <muddman@users.noreply.github.com>
2020-02-14 11:13:02 -0500

    test: lint width

--- scripts/lint-powershell.sh
+++ scripts/lint-powershell.sh
@@ -3,7 +3,7 @@
 pwsh -Command "& {Invoke-ScriptAnalyzer -Path ./scripts/ -Recurse -ReportSummary -Settings ./PSScriptAnalyzerSettings.psd1 |
 Format-Table -Wrap -GroupBy ScriptName -autosize -Property @{ e='Line'; width = 4},
 @{ e='Severity'},
-@{ e='RuleName'; width = 20},
+@{ e='RuleName'; width = 10},
 @{ e='Message'} |
 Out-String -Width 130}"
 # @{ e='ScriptName'; width = 15},
