0c10fe543b0c66aba754ee9e4c1d2d7686147f65
Peter Mudd <muddman@users.noreply.github.com>
2020-02-14 10:17:03 -0500

    test: confirm all programs

--- scripts/uninstall_apps_for_saltstack.ps1
+++ scripts/uninstall_apps_for_saltstack.ps1
@@ -5,19 +5,39 @@
 ### List of likely places where Git could be
 #
 $possibleInstalledPaths = @("C:\Program Files\Git\", "C:\Program Files (x64)\Git\", "c:\git\", "C:\Program Files\Git LFS\", "C:\Program Files\7-Zip\")
+$allPrograms = New-Object System.Collections.Generic.HashSet[int]
+$allProgramsInstallers = New-Object System.Collections.Generic.HashSet[int]
 
 $foundAnInstallation = $false
 ### For all places where Git "could" be.
 foreach ($installPath in $possibleInstalledPaths)
 {
   ### If the path where Git could be exists
+  if ($installPath -like '*Git*')
+  {
+    $program_name = 'Git'
+  }
+  elseif ($installPath -like '*7-Zip*')
+  {
+    $program_name = '7-Zip'
+  }
+  $allPrograms.Add($program_name)
   if (Test-Path($installPath))
   {
+    # # - ps: Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty |
+    # # Where-Object {$_.DisplayName -match "7-Zip"} | Select-Object -Property DisplayName, UninstallString
+    # if (Test-Path "C:\Program Files\7-Zip\Uninstall.exe" -PathType Leaf)
+    # {
+    #   write-host "Removing 7-Zip from C:\Program Files\7-Zip\";
+    #   start-process -FilePath "C:\Program Files\7-Zip\Uninstall.exe" -ArgumentList "/S"
+    # }
+    $allProgramsInstallers.Add($program_name)
     ## Some Git stuff might be running.. kill them.
     Stop-Process -processname Bash -erroraction 'silentlycontinue'
     Stop-Process -processname Putty* -erroraction 'silentlycontinue'
-    $foundAnInstallation = $true
-    Write-Output "Removing Git from " $installPath
+    # $foundAnInstallation = $true
+
+    Write-Output "Removing $program_name from " $installPath
     ### Find if there's an uninstaller in the folder.
     $uninstallers = Get-ChildItem $installPath"\unins*.exe"
     ### In reality, there should only be just one that matches.
@@ -35,37 +55,22 @@ foreach ($installPath in $possibleInstalledPaths)
   }
 }
 # Add confirmation
-if (!($foundAnInstallation))
+
+foreach ($program in $allPrograms)
 {
-   Write-Output "No installation found. Nothing to uninstall"
-}
-else
-{
-  if ($installPath -like '*Git*')
+  if ($program in (set(allPrograms).difference(allProgramsInstallers))))
   {
-    $program_name = 'Git'
+   Write-Output "No uninstallers found for $program."
   }
-  elseif ($installPath -like '*7-Zip*')
-  {
-    $program_name = '7-Zip'
-  }
-  $confirmation = Get-Package -Provider Programs -IncludeWindowsInstaller -Name "$program_name*" -ErrorAction:SilentlyContinue
+  $confirmation = Get-Package -Provider Programs -IncludeWindowsInstaller -Name "$program*" -ErrorAction:SilentlyContinue
   if ($confirmation)
   {
     $host.SetShouldExit($LastExitCode)
     Write-Output $confirmation
-    throw "$program_name not uninstalled"
+    throw "$program not uninstalled"
   }
   else
   {
-    Write-Output "All $program_name instances successfully uninstalled"
+    Write-Output "All $program instances successfully uninstalled"
   }
 }
-
-# # - ps: Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty |
-# # Where-Object {$_.DisplayName -match "7-Zip"} | Select-Object -Property DisplayName, UninstallString
-# if (Test-Path "C:\Program Files\7-Zip\Uninstall.exe" -PathType Leaf)
-# {
-#   write-host "Removing 7-Zip from C:\Program Files\7-Zip\";
-#   start-process -FilePath "C:\Program Files\7-Zip\Uninstall.exe" -ArgumentList "/S"
-# }
